58217c08d1b1ab1728418b7cf1be2b1a6618a35a
Soonmax <ivan.monroy@alu.ucm.cl>
2020-12-31 00:35:22 -0300

    Update ConnectController.php

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Validator;
+use App\usuarios;
 
 class ConnectController extends Controller
 {
@@ -21,17 +22,43 @@ class ConnectController extends Controller
             'name' => 'required',
             'rut' => 'required',
             'apellidos' => 'required',
-            'email' => 'required|email|unique:App\usuarios,Correo ',
+            'email' => 'required|email|unique:usuarios,Correo',
             'telefono' => 'required',
             'direccion' => 'required',
             'password' => 'required|min:8',
-            'cpassword' => 'required|same:password'
+            'cpassword' => 'required|min:8'
         ];
 
-        $validator = Validator::make($request->all(), $rules);
+        $messages = [
+            'name.required' => 'El campo Nombres es requerido',
+            'rut.required' => 'El campo RUT es requerido',
+            'apellidos.required' => 'El campo Apellidos es requerido',
+            'email.required' => 'El campo Correo es requerido',
+            'telefono.required' => 'El campo Teléfono es requerido',
+            'direccion.required' => 'El campo Dirección es requerido',
+            'password.required' => 'El campo Contraseña es requerido',
+            'password.min' => 'La contraseña debe tener al menos 8 carácteres',
+            'cpassword.required' => 'El campo Confirmar Contraseña es requerido',
+            'cpassword.same' => 'Las contraseñas no coinciden',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
         if($validator -> fails()):
             return back()->withErrors($validator) ->with('message','Se ha producido un error','typealert','danger');
         else:
+            $user = new usuarios;
+            $user->Nombres = $request->input('name');
+            $user->RUT = $request->input('rut');
+            $user->Apellidos = $request->input('apellidos');
+            $user->Correo = $request->input('email');
+            $user->Telefono = $request->input('telefono');
+            $user->Dirección = $request->input('direccion');
+            $user->Contrasena = $request->input('password');
+            $user->TipoUsuario = 2;
+
+            if($user->save()):
+                return redirect('/login')->with('message', 'El usuario se ha registrado con éxito')->with('typealert','success');
+            endif;
         endif;
     }
 }
