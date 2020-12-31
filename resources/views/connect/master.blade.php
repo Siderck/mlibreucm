c78071c560ac61c82fd8d051e4bd36fb930fbf57
Soonmax <ivan.monroy@alu.ucm.cl>
2020-12-30 22:22:54 -0300

    Update master.blade.php

--- resources/views/connect/master.blade.php
+++ resources/views/connect/master.blade.php
@@ -6,6 +6,8 @@
     <title>MercadoLibreUCM - @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ url('/static/css/connect.css?v='.time()) }}">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/87216d4d65.js" crossorigin="anonymous"></script>
 </head>
 <body>
